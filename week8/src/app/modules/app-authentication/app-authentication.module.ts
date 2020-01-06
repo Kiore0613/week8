901b76f2933eb149d9a4c666b4d4ef9ac18b7443
Kiore <ricardochicas95@gmail.com>
2020-01-06 14:13:10 -0600

    create components and module shared

--- week8/src/app/modules/app-authentication/app-authentication.module.ts
+++ week8/src/app/modules/app-authentication/app-authentication.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, RegisterComponent, HomeComponent, LayoutComponent],
   imports: [
     CommonModule
   ]
