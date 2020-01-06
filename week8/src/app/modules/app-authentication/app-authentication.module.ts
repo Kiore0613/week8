6a688d15a9c291fefbfe288c5f841355a260ef9c
Kiore <ricardochicas95@gmail.com>
2020-01-06 14:37:47 -0600

    configuration routes

--- week8/src/app/modules/app-authentication/app-authentication.module.ts
+++ week8/src/app/modules/app-authentication/app-authentication.module.ts
@@ -1,16 +1,38 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { HomeComponent } from './components/home/home.component';
-import { LayoutComponent } from './components/layout/layout.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { HomeComponent } from "./components/home/home.component";
+import { LayoutComponent } from "./components/layout/layout.component";
+import { RouterModule } from "@angular/router";
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, HomeComponent, LayoutComponent],
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    LayoutComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    SharedModule,
+    RouterModule.forRoot([
+      {
+        path: "home",
+        component: HomeComponent
+      },
+      {
+        path: "login",
+        component: LoginComponent
+      },
+      {
+        path: "register",
+        component: RegisterComponent
+      }
+    ])
+  ],
+  
+  exports: [LoginComponent, RegisterComponent, HomeComponent, LayoutComponent]
 })
-export class AppAuthenticationModule { }
+export class AppAuthenticationModule {}
