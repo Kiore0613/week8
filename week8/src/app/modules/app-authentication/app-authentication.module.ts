0b6fa60f28b17f12f0b85a40585dc3f27e6fdeab
Kiore <ricardochicas95@gmail.com>
2020-01-06 22:07:19 -0600

    remove layoutComponent

--- week8/src/app/modules/app-authentication/app-authentication.module.ts
+++ week8/src/app/modules/app-authentication/app-authentication.module.ts
@@ -3,19 +3,12 @@ import { CommonModule } from "@angular/common";
 import { LoginComponent } from "./components/login/login.component";
 import { RegisterComponent } from "./components/register/register.component";
 import { HomeComponent } from "./components/home/home.component";
-import { LayoutComponent } from "./components/layout/layout.component";
-import { RouterModule } from "@angular/router";
 import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent,
-    HomeComponent,
-    LayoutComponent
-  ],
+  declarations: [LoginComponent, RegisterComponent, HomeComponent],
   imports: [CommonModule, SharedModule],
 
-  exports: [LoginComponent, RegisterComponent, HomeComponent, LayoutComponent]
+  exports: [LoginComponent, RegisterComponent, HomeComponent]
 })
 export class AppAuthenticationModule {}
--- week8/src/app/modules/shared/services/mock-api.service.ts
+++ week8/src/app/modules/shared/services/mock-api.service.ts
@@ -1,23 +1,35 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { of, Observable } from 'rxjs';
-import {catchError} from "rxjs/operators";
-
+import { of, Observable } from "rxjs";
+import { map, catchError } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
 })
 export class MockApiService {
-
   private baseUrl: string;
   constructor(private http: HttpClient) {
-    this.baseUrl = '/api/login';
+    this.baseUrl = "/api/login";
   }
 
   login(credentials) {
-    return this.http.post(this.baseUrl, JSON.stringify(credentials))
-    .pipe(
-      catchError((error: HttpErrorResponse) => of(null) as Observable<String>)
-  );
+    return this.http
+      .post(this.baseUrl, JSON.stringify(credentials))
+      .pipe(
+        catchError((error: HttpErrorResponse) => of(null) as Observable<String>)
+      );
+  }
+
+  handleError(error) {
+    let errorMessage = "";
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
   }
 }
