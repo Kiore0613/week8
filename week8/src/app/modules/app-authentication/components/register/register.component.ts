6b9cb84c334450981185e6750f382a886b8ffc00
Kiore <ricardochicas95@gmail.com>
2020-01-08 17:04:27 -0600

    add fomrGruop and form builder

--- week8/src/app/modules/app-authentication/components/register/register.component.ts
+++ week8/src/app/modules/app-authentication/components/register/register.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { passwordsValidator } from '../../validators/passwords.validator';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      firstName: this.formBuilder.control('', [Validators.required]),
+      lastName: this.formBuilder.control('', [Validators.required]),
+      username: this.formBuilder.control('', [Validators.required]),
+      email: this.formBuilder.control('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: this.formBuilder.control('', [Validators.required]),
+      confirmPassword: this.formBuilder.control('', [Validators.required]),
+    },
+    {validators: passwordsValidator});
+    
+    this.form.controls.firstName.errors.required;
+    this.form.controls.lastName.errors.required;
+    this.form.controls.username.errors.required;
+    this.form.controls.email.errors.required;
+    this.form.controls.password.errors.required;
+    this.form.controls.confirmPassword.errors.required;
   }
 
+  get firstName() {
+    return this.form.get('firstName');
+  }
+  get lastName() {
+    return this.form.get('lastName');
+  }
+  get username() {
+    return this.form.get('username');
+  }
+  get email() {
+    return this.form.get('email');
+  }
+  get password() {
+    return this.form.get('password');
+  }
+  get confirmPassword() {
+    return this.form.get('confirmPassword');
+  }
+
+  ngOnInit() {}
 }
