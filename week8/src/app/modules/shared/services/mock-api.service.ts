3f45bf028d172849cdca7d33f194105b4a8d64c0
Kiore <ricardochicas95@gmail.com>
2020-01-08 14:02:02 -0600

    refactor login method and remove onDestroy implementation

--- week8/src/app/modules/shared/services/mock-api.service.ts
+++ week8/src/app/modules/shared/services/mock-api.service.ts
@@ -1,41 +1,52 @@
-import { Injectable, ErrorHandler } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { throwError } from "rxjs";
-import { map, catchError } from "rxjs/operators";
-import { UserLogin } from "../../app-authentication/models/login";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { throwError, BehaviorSubject } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+import { UserLogin } from '../../app-authentication/models/login';
 import { loginAuth } from '../../app-authentication/models/loginAuth';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class MockApiService {
   private baseUrl: string;
+  private isLogged = new BehaviorSubject<boolean>(false);
+
   constructor(private http: HttpClient) {
-    this.baseUrl = "https://reqres.in/api/";
+    this.baseUrl = 'https://reqres.in/api/';
   }
 
   login(credentials: loginAuth) {
-    return this.http.post<UserLogin>(`${this.baseUrl}login`, credentials).pipe(
-      map((response: UserLogin) => response),
-      catchError( error => this.handleError(error))
+    return this.http.post<UserLogin>(`${this.baseUrl}login`, credentials)
+    .pipe(
+      tap(() => {
+        this.isLogged.next(true);
+      }),
+      catchError((error) => this.handleError(error))
     );
   }
+
+  logged() {
+    if (localStorage.getItem('token')) {
+      return this.isLogged;
+    }
+  }
+
   handleError(error) {
-    let errorMessage = "";
+    let errorMessage = '';
     console.log(error);
     if (error.error instanceof ErrorEvent) {
       errorMessage = `Error: ${error.error.message}`;
     } else {
       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
-   
     return throwError(errorMessage);
   }
 
   register(userData: loginAuth) {
     return this.http.post<UserLogin>(`${this.baseUrl}register`, userData).pipe(
-      map((response:UserLogin) => response),
+      map((response: UserLogin) => response),
       catchError(this.handleError)
-    )
+    );
   }
 }
