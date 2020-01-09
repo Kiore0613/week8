40f00bb7bfe269ef08d27c56a4f464b91a9affa6
Kiore <ricardochicas95@gmail.com>
2020-01-09 09:56:31 -0600

    add logout method

--- week8/src/app/modules/app-authentication/components/home/home.component.ts
+++ week8/src/app/modules/app-authentication/components/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { MockApiService } from 'src/app/modules/shared/services/mock-api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private mockApiService: MockApiService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  logOut() {
+    this.mockApiService.logout();
+    this.router.navigate(['/login']);
   }
-
 }
