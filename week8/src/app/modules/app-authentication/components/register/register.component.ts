11952f5989d3aa4fc27ca0db66dcb54380dce50b
Kiore <ricardochicas95@gmail.com>
2020-01-10 12:14:46 -0600

    refactor form

--- week8/src/app/modules/app-authentication/components/register/register.component.ts
+++ week8/src/app/modules/app-authentication/components/register/register.component.ts
@@ -1,9 +1,10 @@
+import { RegisterForm } from './../../models/registerForm';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/modules/shared/services/authentication.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { passwordsValidator } from '../../validators/passwords.validator';
-import { HttpErrorResponse } from '@angular/common/http';
+import { CredentialAuth } from '../../models/credential';
 
 @Component({
   selector: 'app-register',
@@ -14,6 +15,7 @@ export class RegisterComponent implements OnInit {
   form: FormGroup;
   credentialForm: FormGroup;
   errorMessage: string;
+  credentials: CredentialAuth;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -25,46 +27,32 @@ export class RegisterComponent implements OnInit {
         firstName: this.formBuilder.control('', [Validators.required]),
         lastName: this.formBuilder.control('', [Validators.required]),
         username: this.formBuilder.control('', [Validators.required]),
+        email: this.formBuilder.control('', [
+          Validators.required,
+          Validators.email,
+        ]),
+        password: this.formBuilder.control('', [Validators.required]),
 
-        credentialForm: this.formBuilder.group({
-          email: this.formBuilder.control('', [
-            Validators.required,
-            Validators.email,
-          ]),
-          password: this.formBuilder.control('', [Validators.required]),
-        }),
         confirmPassword: this.formBuilder.control('', [Validators.required]),
       },
       { validators: passwordsValidator }
     );
   }
 
-  get firstName() {
-    return this.form.get('firstName');
-  }
-  get lastName() {
-    return this.form.get('lastName');
-  }
-  get username() {
-    return this.form.get('username');
-  }
-  get email() {
-    return this.form.get('credentialForm.email');
-  }
-  get password() {
-    return this.form.get('credentialForm.password');
-  }
-  get confirmPassword() {
-    return this.form.get('confirmPassword');
+  getControl(name: string) {
+    return this.form.get(name);
   }
 
   register() {
-    this.authenticationService
-      .register(this.form.get('credentialForm').value)
-      .subscribe(
-        () => this.router.navigate(['/login']),
-        (error: string) => (this.errorMessage = error)
-      );
+    const { email, password } = this.form.value as RegisterForm;
+    this.credentials = {
+      email,
+      password,
+    };
+    this.authenticationService.register(this.credentials).subscribe(
+      () => this.router.navigate(['/login']),
+      (error: string) => (this.errorMessage = error)
+    );
   }
 
   ngOnInit() {}
